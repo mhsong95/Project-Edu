dcf520b9eb0108beb2f77912ea5340725487120e
seoyun.son <sallyluckyyun@gmail.com>
2021-03-17 17:50:49 +0900

    Fix: conflict in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,93 +1,17 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-<<<<<<< HEAD:public/js/script.js
-    host: '/',
-    port: '8080'
-})
+  host: "/",
+  port: "8080",
+});
 
 // The value of this promise is used to broadcast that you've joined the room.
 // Broadcasting occurs when getUserMedia completes, thus all event listeners
 // (e.g. myPeer.on('call')) have had set.
 const myUserIdPromise = new Promise((resolve) => {
-    myPeer.on('open', id => {
-        resolve(id);    // My user ID
-    });
-});
-
-const myVideo = document.createElement('video')
-myVideo.muted = true
-
-const peers = {}
-
-var isCreator = false;  // Indicates whether you have created a room, or just joined
-
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(stream => {
-    addVideoStream(myVideo, stream)
-
-    myPeer.on('call', call => {
-        call.answer(stream)
-        const video = document.createElement('video')
-        call.on('stream', userVideoStream => {
-            addVideoStream(video, userVideoStream)
-        })
-    })
-
-    socket.on('user-connected', userId => {
-        console.log('User connected: ' + userId)
-
-        // Connect to the user only if you are a room creator.
-        if (isCreator) {
-            connectToNewUser(userId, stream)
-        }
-    });
-
-    socket.on('room-created', () => {
-        isCreator = true;
-    });
-
-    myUserIdPromise.then(id => {
-        socket.emit('join-room', ROOM_ID, id);
-    });
-
-})
-
-socket.on('user-disconnected', userId => {
-    if(peers[userId]) peers[userId].close()
-})
-
-function addVideoStream(video, stream) {
-    console.log("new")
-    video.srcObject = stream
-    video.addEventListener('loadedmetadata', () => {
-        video.play()
-    })
-    videoGrid.append(video)
-}
-
-function connectToNewUser(userId, stream) {
-    const call = myPeer.call(userId, stream)
-    const video = document.createElement('video')
-
-    call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
-    })
-    call.on('close', () => {
-        video.remove()
-    })
-
-    peers[userId] = call
-}
-=======
-  host: "/",
-  port: "8080",
-});
-
-myPeer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id);
+  myPeer.on("open", (id) => {
+    resolve(id); // My user ID
+  });
 });
 
 const myVideo = document.createElement("video");
@@ -95,10 +19,7 @@ myVideo.muted = true;
 
 const peers = {};
 
-// chat
-var messages = document.getElementById("messages");
-var form = document.getElementById("form");
-var input = document.getElementById("input");
+var isCreator = false; // Indicates whether you have created a room, or just joined
 
 navigator.mediaDevices
   .getUserMedia({
@@ -117,9 +38,20 @@ navigator.mediaDevices
     });
 
     socket.on("user-connected", (userId) => {
-      sleep(2000);
       console.log("User connected: " + userId);
-      connectToNewUser(userId, stream);
+
+      // Connect to the user only if you are a room creator.
+      if (isCreator) {
+        connectToNewUser(userId, stream);
+      }
+    });
+
+    socket.on("room-created", () => {
+      isCreator = true;
+    });
+
+    myUserIdPromise.then((id) => {
+      socket.emit("join-room", ROOM_ID, id);
     });
   });
 
@@ -139,6 +71,7 @@ function addVideoStream(video, stream) {
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream);
   const video = document.createElement("video");
+
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
@@ -147,11 +80,6 @@ function connectToNewUser(userId, stream) {
   });
 
   peers[userId] = call;
-}
-
-function sleep(ms) {
-  const wakeUpTime = Date.now() + ms;
-  while (Date.now() < wakeUpTime) {}
 }
 
 // chat
@@ -174,4 +102,3 @@ socket.on("message1", function (msg) {
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
 });
->>>>>>> WIP link-chat:public/script.js
