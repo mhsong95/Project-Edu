8952d88c55613aefc538466703c2e0bc44cf527c
seoyun.son <sallyluckyyun@gmail.com>
2021-03-17 16:29:53 +0900

    WIP link-chat

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
+<<<<<<< HEAD:public/js/script.js
     host: '/',
     port: '8080'
 })
@@ -80,3 +81,129 @@ function connectToNewUser(userId, stream) {
 
     peers[userId] = call
 }
+=======
+  host: "/",
+  port: "8080",
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+const peers = {};
+
+// chat
+var messages = document.getElementById("messages");
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+
+// navigator.mediaDevices
+//   .getUserMedia({
+//     video: true,
+//     audio: true,
+//   })
+//   .then((stream) => {
+//     addVideoStream(myVideo, stream);
+
+//     myPeer.on("call", (call) => {
+//       call.answer(stream);
+//       const video = document.createElement("video");
+//       call.on("stream", (userVideoStream) => {
+//         addVideoStream(video, userVideoStream);
+//       });
+//     });
+
+//     socket.on("user-connected", (userId) => {
+//       sleep(2000);
+//       console.log("User connected: " + userId);
+//       connectToNewUser(userId, stream);
+//     });
+//   });
+
+socket.on("user-connected", (userId) => {
+  sleep(2000);
+  console.log("User connected: " + userId);
+  var item = document.createElement("li");
+  connectToNewUser(userId, item);
+});
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+// function addVideoStream(video, stream) {
+//   console.log("new");
+//   video.srcObject = stream;
+//   video.addEventListener("loadedmetadata", () => {
+//     video.play();
+//   });
+//   videoGrid.append(video);
+// }
+
+function addChatMsg(item, msg) {
+  console.log("new msg");
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+// function connectToNewUser(userId, stream) {
+//   const call = myPeer.call(userId, stream);
+//   const video = document.createElement("video");
+//   call.on("stream", (userVideoStream) => {
+//     addVideoStream(video, userVideoStream);
+//   });
+//   call.on("close", () => {
+//     video.remove();
+//   });
+
+//   peers[userId] = call;
+// }
+
+function connectToNewUser(userId, item) {
+  const call = myPeer.call(userId, item);
+  //   const video = document.createElement("video");
+  call.on("item", (i) => {
+    addChatMsg(item);
+  });
+
+  peers[userId] = call;
+}
+
+function sleep(ms) {
+  const wakeUpTime = Date.now() + ms;
+  while (Date.now() < wakeUpTime) {}
+}
+
+// chat
+
+form.addEventListener("submit", function (e) {
+  console.log("eventlistener!");
+  e.preventDefault();
+  if (input.value) {
+    myPeer.on("call", (call) => {
+      var item = document.createElement("li");
+      item.textContent = msg;
+
+      window.scrollTo(0, document.body.scrollHeight);
+      call.on("chatmsg", (chatmsg) => {
+        messages.appendChild(item);
+      });
+    });
+    socket.emit("message", input.value);
+    console.log("listener: " + input.value);
+    input.value = "";
+  }
+});
+
+socket.on("message1", function (msg) {
+  console.log("html socketon");
+  var item = document.createElement("li");
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+>>>>>>> WIP link-chat:public/script.js
--- server.js
+++ server.js
@@ -1,49 +1,54 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* VSCode intellisense does not work
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 */
-const server = require('http').createServer(app);
-const io = new (require('socket.io').Server)(server);
+const server = require("http").createServer(app);
+const io = new (require("socket.io").Server)(server);
 
-const { v4: uuidV4 } = require('uuid')
-var url = require('url')
+const { v4: uuidV4 } = require("uuid");
+var url = require("url");
 
-app.set('view engine', 'ejs')
-app.engine('html', require('ejs').renderFile)
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('../public/index.html');
-})
+app.get("/", (req, res) => {
+  res.render("../public/index.html");
+});
 
-app.get('/create', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
-})
+app.get("/create", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
 
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
-})
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        // If the room roomId does not exist, the user is the creator of the room.
-        // You can signal him/her that you are a creator.
-        if (!io.sockets.adapter.rooms.has(roomId)) {
-            socket.join(roomId);
-            socket.emit('room-created');
-        } else {
-            socket.join(roomId);
-            socket.broadcast.to(roomId).emit('user-connected', userId);
-        }
-        console.log(roomId, userId)
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    // If the room roomId does not exist, the user is the creator of the room.
+    // You can signal him/her that you are a creator.
+    if (!io.sockets.adapter.rooms.has(roomId)) {
+      socket.join(roomId);
+      socket.emit("room-created");
+    } else {
+      socket.join(roomId);
+      socket.broadcast.to(roomId).emit("user-connected", userId);
+    }
+    console.log(roomId, userId);
 
-        socket.on('disconnect', () => {
-            socket.broadcast.to(roomId).emit('user-disconnected', userId)
-        })
-    })
-})
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
 
-server.listen(8000)
+  socket.on("message", (msg) => {
+    console.log("server socket msg" + msg);
+    io.sockets.emit("message1", msg);
+  });
+});
+
+server.listen(8000);
