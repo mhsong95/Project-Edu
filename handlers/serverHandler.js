b1b2e89b88b6d2c1c2d42042a0a05cf600a1f031
1a2babystar <55686526+1a2babystar@users.noreply.github.com>
2021-04-12 20:06:52 +0900

    update3

--- handlers/serverHandler.js
+++ handlers/serverHandler.js
@@ -6,6 +6,7 @@ const { Server, Socket } = require("socket.io");
 // Require database and models for Room and Participant.
 const rooms = require("../db");
 const { Participant } = require("../models/room");
+const {v4: uuidV4} = require("uuid");
 
 /**
  * Registers event handlers on server side event.
@@ -14,10 +15,16 @@ const { Participant } = require("../models/room");
  */
 module.exports = function (io, socket) {
   // Client sent "message" when user click submit button. Sever sends it back to all client.
-  socket.on("message", (roomId, msg, name) => {
+  socket.on("message", (roomId, type, uuid,  msg, name) => {
     console.log("server socket msg " + msg);
+    if(type == "question"){
+      io.sockets.to(roomId).emit("question", `${uuidV4()}`, msg, name);
+    }
+    else if(type == "answer"){
+      io.sockets.to(roomId).emit("answer", uuid, msg, name);
+    }
 
-    io.sockets.to(roomId).emit("message1", msg, name);
+    //io.sockets.to(roomId).emit("message1", msg, name);
   });
 
   // get concentrate data from students
