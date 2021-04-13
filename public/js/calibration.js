000fc0b21b102be65674290e8ffda2e7f8d51645
seoyun.son <sallyluckyyun@gmail.com>
2021-04-14 00:25:06 +0900

    Hide webgazer viderpreview & predictionpoints

--- public/js/calibration.js
+++ public/js/calibration.js
@@ -1,10 +1,10 @@
 var PointCalibrate = 0;
-var CalibrationPoints={};
+var CalibrationPoints = {};
 
 /**
  * Clear the canvas and the calibration button.
  */
-function ClearCanvas(){
+function ClearCanvas() {
   $(".Calibration").hide();
   var canvas = document.getElementById("plotting_canvas");
 }
@@ -12,7 +12,7 @@ function ClearCanvas(){
 /**
  * Show the instruction of using calibration at the start up screen.
  */
-function PopUpInstruction(){
+function PopUpInstruction() {
   ClearCanvas();
   ShowCalibrationPoint();
 }
@@ -26,21 +26,21 @@ function ShowCalibrationPoint() {
 }
 
 /**
-* This function clears the calibration buttons memory
-*/
-function ClearCalibration(){
+ * This function clears the calibration buttons memory
+ */
+function ClearCalibration() {
   // Clear data from WebGazer
 
-  $(".Calibration").css('background-color','red');
-  $(".Calibration").css('opacity',0.2);
-  $(".Calibration").prop('disabled',false);
+  $(".Calibration").css("background-color", "red");
+  $(".Calibration").css("opacity", 0.2);
+  $(".Calibration").prop("disabled", false);
 
   CalibrationPoints = {};
   PointCalibrate = 0;
 }
 
 // sleep function because java doesn't have one, sourced from http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-function sleep (time) {
+function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
@@ -48,56 +48,59 @@ let send = false;
 
 /**
  * Load this function when the index page starts.
-* This function listens for button clicks on the html page
-* checks that all buttons have been clicked 5 times each, and then goes on to measuring the precision
-*/
-$(document).ready(function(){
+ * This function listens for button clicks on the html page
+ * checks that all buttons have been clicked 5 times each, and then goes on to measuring the precision
+ */
+$(document).ready(function () {
   ClearCanvas();
-     $(".Calibration").click(function(){ // click event on the calibration buttons
+  $(".Calibration").click(function () {
+    // click event on the calibration buttons
 
-      var id = $(this).attr('id');
+    var id = $(this).attr("id");
 
-      if (!CalibrationPoints[id]){ // initialises if not done
-        CalibrationPoints[id]=0;
-      }
-      CalibrationPoints[id]++; // increments values
+    if (!CalibrationPoints[id]) {
+      // initialises if not done
+      CalibrationPoints[id] = 0;
+    }
+    CalibrationPoints[id]++; // increments values
 
-      if (CalibrationPoints[id]==5){ //only turn to yellow after 5 clicks
-        $(this).css('background-color','yellow');
-        $(this).prop('disabled', true); //disables the button
-        PointCalibrate++;
-      }else if (CalibrationPoints[id]<5){
-        //Gradually increase the opacity of calibration points when click to give some indication to user.
-        var opacity = 0.2*CalibrationPoints[id]+0.2;
-        $(this).css('opacity',opacity);
-      }
+    if (CalibrationPoints[id] == 5) {
+      //only turn to yellow after 5 clicks
+      $(this).css("background-color", "yellow");
+      $(this).prop("disabled", true); //disables the button
+      PointCalibrate++;
+    } else if (CalibrationPoints[id] < 5) {
+      //Gradually increase the opacity of calibration points when click to give some indication to user.
+      var opacity = 0.2 * CalibrationPoints[id] + 0.2;
+      $(this).css("opacity", opacity);
+    }
 
-      //Show the middle calibration point after all other points have been clicked.
-      if (PointCalibrate == 8){
-        $("#Pt5").show();
-      }
+    //Show the middle calibration point after all other points have been clicked.
+    if (PointCalibrate == 8) {
+      $("#Pt5").show();
+    }
 
-      if (PointCalibrate >= 9){ // last point is calibrated
-        //using jquery to grab every element in Calibration class and hide them except the middle point.
-        $("#calib").hide();
-        $("#webgazerVideoContainer").hide();
-        $("#main").show();
-        send = true;
-        // webgazer.showPredictionPoints(false);
-      }
-    });
+    if (PointCalibrate >= 9) {
+      // last point is calibrated
+      //using jquery to grab every element in Calibration class and hide them except the middle point.
+      $("#calib").hide();
+      $("#webgazerVideoContainer").hide();
+      $("#main").show();
+      send = true;
+      webgazer.showPredictionPoints(false);
+    }
+  });
 });
 
-function skip(){
+function skip() {
   $("#calib").hide();
-  // $("#webgazerVideoContainer").hide();
+  $("#webgazerVideoContainer").hide();
   $("#main").show();
-  webgazer.showVideoPreview(true).showPredictionPoints(true);
+  webgazer.showVideoPreview(false).showPredictionPoints(false);
   send = true;
 }
 
-window.onload = async function() {
-
+window.onload = async function () {
   $("#main").hide();
 
   Restart();
@@ -111,72 +114,71 @@ window.onload = async function() {
   let startLookTime = Number.POSITIVE_INFINITY;
   let lookDirection = null;
 
-  await webgazer.setRegression('ridge') /* currently must set regression and tracker */
-      //.setTracker('clmtrackr')
-      .setGazeListener(function(data, timestamp) {
-       //console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-        //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
-        const videogrid = document.getElementById("screen-video");
-        const left = videogrid.offsetLeft;
-        const right = videogrid.offsetLeft + videogrid.offsetWidth;
-        const top = videogrid.offsetTop;
-        const bottom = videogrid.offsetTop + videogrid.offsetHeight;
+  await webgazer
+    .setRegression("ridge") /* currently must set regression and tracker */
+    //.setTracker('clmtrackr')
+    .setGazeListener(function (data, timestamp) {
+      //console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+      //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+      const videogrid = document.getElementById("screen-video");
+      const left = videogrid.offsetLeft;
+      const right = videogrid.offsetLeft + videogrid.offsetWidth;
+      const top = videogrid.offsetTop;
+      const bottom = videogrid.offsetTop + videogrid.offsetHeight;
 
-        if (data == null || lookDirection === "STOP") return;
+      if (data == null || lookDirection === "STOP") return;
 
-        if (
-          data.x >= left &&
-          data.x <= right &&
-          data.y >= top &&
-          data.y <= bottom
-        ) {
-          // videogrid.style.backgroundColor = "blue";
-          if (send && (lookDirection !== null) ){
-            add_concentrate_log(timestamp, 10);
-          }
-          startLookTime = Number.POSITIVE_INFINITY; // restart timer
-          lookDirection = null;
-        } else if (lookDirection !== "RESET" && lookDirection === null) {
-          // videogrid.style.backgroundColor = "yellow";
-          startLookTime = timestamp;
-          lookDirection = "OUT";
-          if (send) add_concentrate_log(timestamp, 5);
+      if (
+        data.x >= left &&
+        data.x <= right &&
+        data.y >= top &&
+        data.y <= bottom
+      ) {
+        // videogrid.style.backgroundColor = "blue";
+        if (send && lookDirection !== null) {
+          add_concentrate_log(timestamp, 10);
         }
+        startLookTime = Number.POSITIVE_INFINITY; // restart timer
+        lookDirection = null;
+      } else if (lookDirection !== "RESET" && lookDirection === null) {
+        // videogrid.style.backgroundColor = "yellow";
+        startLookTime = timestamp;
+        lookDirection = "OUT";
+        if (send) add_concentrate_log(timestamp, 5);
+      }
 
-        if (startLookTime + LOOK_DELAY < timestamp) {
-          console.log(left, right, top, bottom);
-          // videogrid.style.backgroundColor = "red";
-          if (send) add_concentrate_log(timestamp, 0);
+      if (startLookTime + LOOK_DELAY < timestamp) {
+        console.log(left, right, top, bottom);
+        // videogrid.style.backgroundColor = "red";
+        if (send) add_concentrate_log(timestamp, 0);
 
-          startLookTime = Number.POSITIVE_INFINITY;
-          lookDirection = "STOP";
-          setTimeout(() => {
-            lookDirection = "RESET";
-          }, 200);
-        }
-      }).begin();
+        startLookTime = Number.POSITIVE_INFINITY;
+        lookDirection = "STOP";
+        setTimeout(() => {
+          lookDirection = "RESET";
+        }, 200);
+      }
+    })
+    .begin();
 };
 
 // Set to true if you want to save the data even if you reload the page.
 window.saveDataAcrossSessions = true;
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   webgazer.end();
-}
+};
 
 /**
-* Restart the calibration process by clearing the local storage and reseting the calibration point
-*/
-function Restart(){
+ * Restart the calibration process by clearing the local storage and reseting the calibration point
+ */
+function Restart() {
   webgazer.clearData();
   ClearCalibration();
   PopUpInstruction();
 }
 
-
-
-
-function store_points_variable(){
+function store_points_variable() {
   webgazer.params.storingPoints = true;
 }
 
@@ -184,6 +186,6 @@ function store_points_variable(){
  * Sets store_points to false, so prediction points aren't
  * stored any more
  */
-function stop_storing_points_variable(){
+function stop_storing_points_variable() {
   webgazer.params.storingPoints = false;
 }
